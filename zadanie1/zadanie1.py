adc1f541308e9f2c48ebf14f03aa61934ffafbe5
tomaszd <tomaszdudziec@gmil.com>
2014-03-23 09:48:57 +0100

    slowniki konwesrji B32-> dec dec->B32

--- zadanie1/zadanie1.py
+++ zadanie1/zadanie1.py
@@ -5,3 +5,17 @@ import common
 '''tutaj leci kod''' 
 
 """wczytane dane = common.load_data('cos')"""
+
+B32_2_dec={'a':0,'b':1,'c':2,'d':3,'e':4,'f':5,'g':6,'h':7,
+     'k':8,'l':9,'m':10,'n':11,'p':12,'q':13,'r':14,
+     's':15,'A':16,'B':17,'C':18,'D':19,'E':20,'F':21,
+     'G':22,'H':23,'K':24,'L':25,'M':26,'N':27,'P':28,
+     'Q':29,'R':30,'S':31
+     }
+
+dec_2_B32= dict (zip(B32_2_dec.values(),B32_2_dec.keys()))
+
+
+    
+
+    
